99626533294fd38fa15a2d0aa897255aa750864b
Jess Olson <jess.olson@utah.edu>
2026-06-24 17:29:49 -0600

    fixed?

--- js/fit-screen.js
+++ js/fit-screen.js
@@ -4,13 +4,12 @@ function fitGameToViewport() {
 
   const designWidth = 1500;
   const designHeight = 923;
-  const comfortShrink = 0.88;
 
   const scale = Math.min(
     window.innerWidth / designWidth,
     window.innerHeight / designHeight,
     1
-  ) * comfortShrink;
+  );
 
   app.style.setProperty("--fit-scale", String(scale));
 }
